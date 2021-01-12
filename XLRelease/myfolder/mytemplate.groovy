b82737f81c15ebb237f3c24e10cee6ab79c4cf74
Amit Mohleji <amit.mohleji@gmail.com>
2021-01-12 16:40:27 -0500

    v2

--- XLRelease/myfolder/mytemplate.groovy
+++ XLRelease/myfolder/mytemplate.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Amits-MacBook-Pro.local:5516/#/templates/Folder30517ef2dfbf4370a24c0f9f6f8d89c9-Release54ad8f71eae743bc83b87a87a07c2cc0/releasefile
 // Release version:      9.8.0
-// Date created:         Tue Jan 12 16:36:48 EST 2021
+// Date created:         Tue Jan 12 16:40:26 EST 2021
 
 xlr {
   template('mytemplate') {
@@ -8,7 +8,11 @@ xlr {
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2021-01-12T09:00:00-0500')
     phases {
       phase('New Phase') {
-        
+        tasks {
+          manual('mt1') {
+            
+          }
+        }
       }
     }
     
